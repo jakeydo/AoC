1ad2eb236a87d43f9b522a9bd202485ab23564e4
Jake Spencer <various@jakespencer.net>
2023-12-09 08:53:33 -0600

    day 9 done!

--- AoC2023/AoC2023d09/main.py
+++ AoC2023/AoC2023d09/main.py
@@ -42,14 +42,42 @@ def extrapolate(row):
             triangle[row_index].append(this_row_last+next_row_last)
 
     return triangle[0][-1]
+
+def reverse_extrapolate(row):
+    row = row[:]
+    row_index = 0
+    triangle = [row]
+    while not all(x==0 for x in triangle[row_index]):
+        row = triangle[row_index]
+        next_row = []
+        for i in range(1,len(row)):
+            next_row.append(row[i]-row[i-1])
+        triangle.append(next_row)
+        row_index +=1
+        
+       
+    tri_length = len(triangle)
+    for i in range(tri_length):
+        row_index = -(i+1)
+        if row_index == -1:
+            triangle[row_index].append(0)
+        else:
+            next_row_first = triangle[row_index+1][0]
+            this_row_first = triangle[row_index][0]
+            triangle[row_index].insert(0,this_row_first-next_row_first)
+
+    return triangle[0][0]
         
 new_ends = []
+new_beginnings = []
 for r in readings:
     new_ends.append(extrapolate(r))
+    new_beginnings.append(reverse_extrapolate(r))
+
 
 #put the code here
 
 print(f"Part 1 answer: {sum(new_ends)}")
 
 
-print(f"Part 2 answer: {0}")
+print(f"Part 2 answer: {sum(new_beginnings)}")
