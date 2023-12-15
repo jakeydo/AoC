424d03bb0ede235475af9126541d0eea9c14c6a7
Jake Spencer <various@jakespencer.net>
2023-12-15 09:18:13 -0600

    gives right answers for part 1 and 2 or day 13, with a lot of extra junk printed out

--- AoC2023/AoC2023d13/main.py
+++ AoC2023/AoC2023d13/main.py
@@ -21,12 +21,16 @@ for line in lines:
         block.append(line.strip())
 blocks.append(block)
 
-total = 0
-for b in blocks:
+def horizontal_reflection(b):
     #check reflection around horizontal line
+    print("finding hr")
+    for l in b:
+        disp = ""
+        for c in l:
+            disp+= c
+        print(disp)
+    print()
     b_size = len(b)
-    h_reflect = False
-    h_reflect_rows = 0
     for x in range(b_size-1):
         left = b[:x+1]
         sub_size = len(left)
@@ -37,15 +41,20 @@ for b in blocks:
             left = left[left_biggerness:]
         right.reverse()
         if left == right:
-            h_reflect = True
-            h_reflect_rows = x+1
+            return x+1
+    return 0
 
-    #check reflection around vertical line
-    b = list(map(list,zip(*b)))
+def vertical_reflection(b):
+    print("finding vertical")
+    bprime = list(map(list,zip(*b)))
+    return horizontal_reflection(bprime)
+
+def find_smudge(b):
+    print("finding smudge in block")
+    return_val = 0
     b_size = len(b)
-    v_reflect = False
-    v_reflect_rows = 0
     for x in range(b_size-1):
+        print(f"checking reflect on row:{x+1}")
         left = b[:x+1]
         sub_size = len(left)
         right_end = min(b_size,x+1+sub_size)
@@ -54,21 +63,96 @@ for b in blocks:
         if left_biggerness > 0:
             left = left[left_biggerness:]
         right.reverse()
-        if left == right:
-            v_reflect = True
-            v_reflect_rows = x+1
-    
-    if h_reflect:
-        print(f"h reflection with {h_reflect_rows} rows")
-        total += 100*h_reflect_rows
         
-    if v_reflect:
-        print(f"v reflection with {v_reflect_rows} rows")
-        total += v_reflect_rows
+        diff_count = 0
+        last_diff = False
+        
+        for x in range(len(left)):
+            return_val = (len(left)+left_biggerness) * 100
+            for y in range(len(left[x])):
+                if left[x][y] != right[x][y]:
+                    diff_count +=1
+                    last_diff = (left_biggerness+x,y)
+                    print(f"diff count:{diff_count}, loc:{(x,y)}")
+                    if diff_count > 1:
+                        last_diff = False
+                        break
+            if diff_count > 1:
+                break
+        if last_diff:
+            print(return_val)
+            #return last_diff
+            return return_val
+
+    bprime = b[:]
+    bprime = list(map(list,zip(*b)))
+    b_size = len(bprime)
+    for x in range(b_size-1):
+        print(f"checking reflect on row:{x+1}")
+        left = bprime[:x+1]
+        sub_size = len(left)
+        right_end = min(b_size,x+1+sub_size)
+        right = bprime[x+1:right_end]
+        left_biggerness = len(left) - len(right)
+        if left_biggerness > 0:
+            left = left[left_biggerness:]
+        right.reverse()
+        
+        diff_count = 0
+        last_diff = False
+        for x in range(len(left)):
+            return_val = len(left)+left_biggerness
+            for y in range(len(left[x])):
+                if left[x][y] != right[x][y]:
+                    diff_count +=1
+                    last_diff = (left_biggerness+x,y)
+                    print(f"diff count:{diff_count}, loc:{(x,y)}")
+                    if diff_count > 1:
+                        last_diff = False
+                        break
+            if diff_count > 1:
+                break
+        if last_diff:
+            print(return_val)
+            print(f"smudge at: {last_diff}")
+            #return last_diff
+            return return_val
+    print(bprime)
+
+    #return False
 
 #put the code here
 
+total = 0
+total2 = 0
+
+for i,b in enumerate(blocks):
+    
+    print(f"about to check unmessed with block {i}")
+    hr = horizontal_reflection(b)
+    vr = vertical_reflection(b)
+    print(f"b hr:{hr} vr:{vr}")
+    total += 100*hr + vr
+    """
+    x,y = smudge
+    bprime = b[:]
+    bprime[x] = list(map(str,bprime[x]))
+    if bprime[x][y] == "#":
+        bprime[x][y] = "."
+    else:
+        bprime[x][y] = "#"
+    print("now on bprime")
+    hr = horizontal_reflection(bprime)
+    vr = vertical_reflection(bprime)
+    print(f"bprime hr:{hr} vr:{vr}")
+    total2 += 100*hr + vr 
+    """
+    
+    smudge = find_smudge(b)
+    total2 += smudge
+    print(f"smudge value: {smudge}")
+    print()
+
 print(f"Part 1 answer: {total}")
 
-
-print(f"Part 2 answer: {0}")
+print(f"Part 2 answer: {total2}")
