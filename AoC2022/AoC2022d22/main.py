681df21d9c13544087c043f0e0fc50121493464f
Jake Spencer <various@jakespencer.net>
2023-12-19 15:19:22 -0600

    checking in some 2022 stuff

--- AoC2022/AoC2022d22/main.py
+++ AoC2022/AoC2022d22/main.py
@@ -1,5 +1,5 @@
 import sys
-#from collections import defaultdict
+from collections import defaultdict
 #from os import system
 #import time
 #from functools import cache
@@ -11,8 +11,23 @@ else:
     f = open("real.txt")
 lines = f.readlines()
 
-for line in lines:
-    pass
+directions = lines[-1]
+lines = lines[:-2]
+
+world = defaultdict(lambda:" ")
+
+x_size = len(lines)
+y_size = max([len(x) for x in lines])
+
+for x in range(x_size):
+    for y in range(len(lines[x])-1):
+        world[(x,y)] = lines[x][y]
+
+start_y = min([y for x,y in world if x==0 and world[(x,y)]=="."])
+start = (0,start_y)
+
+#0 -> right, 1 -> down, 2 -> left, 3 -> up
+facing = 0
 
 #put the code here
 
