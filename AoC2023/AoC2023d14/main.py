e13e9b9c4f3ef94747058d4a104dd7bb15499eb4
Jake Spencer <various@jakespencer.net>
2023-12-19 10:46:37 -0600

    got day 14 both parts in a pretty package

--- AoC2023/AoC2023d14/main.py
+++ AoC2023/AoC2023d14/main.py
@@ -1,5 +1,6 @@
 import sys
 from collections import defaultdict
+import copy
 #from os import system
 #import time
 #from functools import cache
@@ -14,11 +15,15 @@ lines = f.readlines()
 rocks = defaultdict(lambda:".")
 index = 0
 
+rocks2 = []
+
 number_lines = len(lines)
 number_chars = len(lines[0].strip())
 
 for x, line in enumerate(lines):
+    row = []
     for y, c in enumerate(line.strip()):
+        row.append(c)
         if c != ".":
             rocks[index] = {'x':x, 'y':y, 'kind':c}
             if c == "#":
@@ -26,6 +31,7 @@ for x, line in enumerate(lines):
             else:
                 rocks[index]['stopped'] = False
             index +=1
+    rocks2.append(row)
 
 def can_move(position,rocks):
     x,y = position
@@ -40,6 +46,7 @@ def can_move(position,rocks):
     else:
         #print("return false")
         return False
+
 
 def print_rocks(rocks,nl,nc):
     for x in range(nl):
@@ -60,6 +67,8 @@ def print_rocks(rocks,nl,nc):
             
 #print_rocks(rocks,number_lines,number_chars)
 
+
+"""
 for x in range(len(lines)):
     movers = [r for r in rocks if (not rocks[r]['stopped'] and rocks[r]['x'] == x)]
     #print(f"line {x}")
@@ -88,7 +97,195 @@ for r in rollers:
     total += force_factor
     x,y = r['x'],r['y']
     #print(f"x:{x} y:{y} ff:{force_factor}")
-print(f"Part 1 answer: {total}")
+"""
+
+def print_rocks2(rocks):
+    print()
+    print("***")
+    for x in range(len(rocks)):
+        print(rocks[x])
+    print()
+    
+def roll_rock(rocks,position,direction):
+    x1,y1= position
+    x2,y2= direction
+    
+    newx = x1+x2
+    newy = y1+y2
+    
+    #print(f"direction:{direction}")
+    #if direction == (1,0):
+    #    print(f"newx:{newx} newy:{newy}")
+    
+    if rocks[x1][y1] == "O" and newx in range(len(rocks)) and newy in range(len(rocks[0])) and rocks[newx][newy] == ".":
+        rocks[x1][y1] = "."
+        rocks[newx][newy] = "O"
+    #    if direction == (1,0):
+    #        print_rocks2(rocks)
+        roll_rock(rocks,(newx,newy),direction)
+    else:
+        return False
+
+total = 0
+
+rocks = copy.deepcopy(rocks2)
+d = (-1,0)
+xr = range(len(rocks))
+yr = range(len(rocks[0]))
+if d[0] == 1:
+    xr = reversed(xr)
+if d[1] == 1:
+    yr = reversed(yr)
+#print(f"direction:{d}")
+for x in xr:
+    yr = range(len(rocks[0]))
+    if d[1] == 1:
+        yr = reversed(yr)
+    for y in yr:
+        #print(f"y loop x:{x} y:{y}")
+        roll_rock(rocks,(x,y),d)
+
+total = 0
+x_size = len(rocks)
+y_size = len(rocks[0])
+for x in range(x_size):
+    force_factor = x_size - x
+    for y in range(y_size):
+        if rocks[x][y] == "O":
+            total += force_factor
+print(f"part 1 answer: {total}")
+
+#print_rocks2(rocks2)
+
+directions = [(-1,0),(0,-1),(1,0),(0,1)]
+
+breakout = False
+
+for d in directions:
+    xr = range(len(rocks2))
+    yr = range(len(rocks2[0]))
+    
+    if d[0] == 1:
+        xr = reversed(xr)
+    if d[1] == 1:
+        yr = reversed(yr)
+    
+    
+    #print(f"direction:{d}")
+    for x in xr:
+        yr = range(len(rocks2[0]))
+        if d[1] == 1:
+            yr = reversed(yr)
+        for y in yr:
+            #print(f"y loop x:{x} y:{y}")
+            roll_rock(rocks2,(x,y),d)
+#print_rocks2(rocks2)
+
+original = copy.deepcopy(rocks2)
+i = 1
+cycle_results = [original]
+repeated_indices = set()
+
+while not breakout:
+    #print(f"cycle {i}")
+    total2 = 0
+    x_size = len(rocks2)
+    y_size = len(rocks2[0])
+    for x in range(x_size):
+        force_factor = x_size - x
+        for y in range(y_size):
+            if rocks2[x][y] == "O":
+                total2 += force_factor
+    #print(f"after {i} cycles, load: {total2}")
+    for d in directions:
+        xr = range(len(rocks2))
+        yr = range(len(rocks2[0]))
+        
+        if d[0] == 1:
+            xr = reversed(xr)
+        if d[1] == 1:
+            yr = reversed(yr)
+        
+        
+        #print(f"direction:{d}")
+        for x in xr:
+            yr = range(len(rocks2[0]))
+            if d[1] == 1:
+                yr = reversed(yr)
+            for y in yr:
+                #print(f"y loop x:{x} y:{y}")
+                roll_rock(rocks2,(x,y),d)
+    #print_rocks2(rocks2)
+    match_count = 0
+    for ri,result in enumerate(cycle_results):
+        #print(f"ri:{ri}")
+        match = True
+        for x in range(len(rocks2)):
+            for y in range(len(rocks2[0])):
+                if result[x][y] != rocks2[x][y]:
+                    match = False
+                    break
+            if not match:
+                break
+        if match:
+            match_count +=1
+            if match_count > len(repeated_indices):
+                repeated_indices.add(i)
+            #print(f"cycle repeated after {i} cycles")
+            if len(repeated_indices) > 2:
+                breakout = True
+            
+    cycle_results.append(copy.deepcopy(rocks2))
+    i = i+1
+
+repeated_indices = list(repeated_indices)
+repeated_indices.sort()
+#print(repeated_indices)
+x,x1,x2 = repeated_indices
+r = x2-x1
+y=1000000000-x1
+y2=y//r*r
+a=y-y2
+right_rocks = x1+a
+
+right_rocks = cycle_results[right_rocks - 1]
+total2 = 0
+x_size = len(right_rocks)
+y_size = len(right_rocks[0])
+for x in range(x_size):
+    force_factor = x_size - x
+    for y in range(y_size):
+        if right_rocks[x][y] == "O":
+            total2 += force_factor
+
+print(f"Part 2 answer: {total2}")    
+    
+    
+"""
+for x in range(len(rocks2)):
+    for y in range(len(rocks2[0])):
+        roll_rock(rocks2,(x,y),(-1,0))
+
+print_rocks2(rocks2)
+
+for x in range(len(rocks2)):
+    for y in range(len(rocks2[0])):
+        roll_rock(rocks2,(x,y),(0,-1))
+
+print_rocks2(rocks2)
+
+for x in range(len(rocks2)):
+    for y in range(len(rocks2[0])):
+        roll_rock(rocks2,(x,y),(1,0))
+
+print_rocks2(rocks2)
+
+for x in range(len(rocks2)):
+    for y in range(len(rocks2[0])):
+        roll_rock(rocks2,(x,y),(0,1))        
+
+print_rocks2(rocks2)
+"""
 
 
-print(f"Part 2 answer: {0}")
+
