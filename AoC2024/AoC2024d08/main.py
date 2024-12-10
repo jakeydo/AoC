ab34b3c2a22721e2723fa93759340e2f6fa1bb23
Jake Spencer <various@jakespencer.net>
2024-12-10 13:06:58 -0600

    24d8 solved

--- AoC2024/AoC2024d08/main.py
+++ AoC2024/AoC2024d08/main.py
@@ -1,4 +1,5 @@
 import sys
+from copy import deepcopy
 #from collections import defaultdict
 #from os import system
 #import time
@@ -11,114 +12,65 @@ else:
     f = open("real.txt")
 lines = f.readlines()
 
-tvs = []
-eqs = [] 
+freqs = set()
+freq_locs = {}
+grid = []
+antinodes = set()
+p2_antinodes = set()
 
-for line in lines:
-    tv, eq = line.strip().split(": ")
-    tv = int(tv)
-    eq = eq.split()
-    for i,e in enumerate(eq):
-        eq[i] = e
-    tvs.append(tv)
-    eqs.append(eq)
+for i,line in enumerate(lines):
+    line = line.strip()
+    grid.append(line)
+    for j,c in enumerate(line):
+        if not (c == "."):
+            if not (c in freqs):
+                freq_locs[c] = set()
+            freqs.add(c)            
+            freq_locs[c].add((i,j))
 
-tcv = 0
-bad_eqs = []
+max_x = len(grid)
+max_y = len(grid[0])
 
-for i in range(len(tvs)):
-    good = False
-    tv = tvs[i]
-    eq = eqs[i]
-    number_operators = (len(eqs[i])-1) 
-    combo_count = (2**number_operators)      
-
+for f in freqs:
+    locs = freq_locs[f]
+    for l in locs:
+        other_locs = deepcopy(locs)
+        other_locs.remove(l)
+        for o in other_locs:
+            lx,ly = l
+            ox,oy = o
+            dx = lx-ox
+            dy = ly-oy
         
-    for j in range(combo_count):
-        if not good:
-            e_str = ""
-            #bin_count = format(number_operators,bin(i))
-            bin_count = format(j,f"0{number_operators}b")
-            #print(bin_count)
-            bin_count = bin_count.replace("0","+").replace("1","*")
+            newx = lx+dx
+            newy = ly+dy
+        
+            if (newx in range(max_x)) and (newy in range(max_y)):
+                antinodes.add((newx,newy))
+                #print(f"l:{l} o:{o}, new:{(newx,newy)}")  
             
-            value = eq[0]
-            for k in range(number_operators):
-                e_str = str(value) + bin_count[k] + eq[k+1]
-                value = eval(e_str)
-                #if j < number_operators:
-                #    e_str = e_str + bin_count[j]
-            #print(e_str)
-            #ev = eval(e_str)
-            #print(ev)
-            ev = value
-            #print(ev)
-            if tv == ev:
-                tcv += tv
-                #print(f"adding:{tv} ")
-                #print(f"total tcv: {tcv}")
-                good = True
-    if not good:
-        bad_eqs.append(i)
-    
-  
+            for m in range(-max_x,max_x+1):
+                newx = lx+m*dx
+                newy = ly+m*dy
+            
+                if (newx in range(max_x)) and (newy in range(max_y)):
+                    p2_antinodes.add((newx,newy))
+                    #print(f"l:{l} o:{o}, new:{(newx,newy)}")
+"""
+for line in grid:
+    print(line)
 
+for c in freqs:
+    print(c)
+    print(freq_locs[c])
+    print()
+
+for a in antinodes:
+    print(a)
+"""
 #put the code here
 
-print(f"Part 1 answer: {tcv}")
-
-def ternary (n):
-    if n == 0:
-        return '0'
-    nums = []
-    while n:
-        n, r = divmod(n, 3)
-        nums.append(str(r))
-    return ''.join(reversed(nums))
+print(f"Part 1 answer: {len(antinodes)}")
 
 
-for i in bad_eqs:
-    eq = eqs[i]    
-    tv = tvs[i]
-    good = False
-    
-    number_operators = (len(eqs[i])-1) 
-    combo_count = (3**number_operators)
-    
-    for j in range(combo_count):
-        if not good:
-            e_str = ""
-            value = eq[0]
-            
-            
-            #ter_count = format(j,f"0{number_operators}b")
-            ter_count = ternary(j).zfill(number_operators)
-            #print(bin_count)
-            ter_count = ter_count.replace("0","+").replace("1","*").replace("2","|")
-            
-            value = eq[0]
-            #print(f"tv:{tv} eq:{eq}  ter_count:{ter_count}")
-            for k in range(number_operators):
-                if ter_count[k] == "|":
-                    value = eval(str(value) + eq[k+1])
-                else:
-                    e_str = str(value) + ter_count[k] + eq[k+1]
-                    #print(f"es:{e_str}")
-                    value = eval(e_str)
-                    #print(f"value:{value}")
-                #if j < number_operators:
-                #    e_str = e_str + bin_count[j]
-            #print(e_str)
-            #ev = eval(e_str)
-            #print(ev)
-            ev = value
-            
-            #print(ev)
-            if tv == ev:
-                tcv += tv
-                #print(f"adding:{tv} ")
-                #print(f"total tcv: {tcv}")
-                good = True
-    
-    
-print(f"Part 2 answer: {tcv}")
+print(f"Part 2 answer: {len(p2_antinodes)}")
